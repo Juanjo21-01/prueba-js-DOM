cce8f3daad138259b1b2c61ba1ef1d363c262c70
Juan Jose <chevike15@gmail.com>
2022-08-07 21:55:01 -0600

    clase03 terminada

--- js/index.js
+++ js/index.js
@@ -5,6 +5,23 @@ const createTask = (evento) => {
   //quitar por defecto  el evento de recargar la pagina
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  input.value = '';
+
+  const tarea = document.createElement('li');
+  //le agregamos la clase
+  tarea.classList.add('card');
+  //backticks
+  const contenido = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+  tarea.innerHTML = contenido;
+
+  const agregarLista = document.querySelector('[data-list]');
+  agregarLista.appendChild(tarea);
 };
 
 //Arrow functions o funciones flechas o funciones anonimas =>
